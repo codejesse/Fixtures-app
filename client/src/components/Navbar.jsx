ccb1b28de2f8263211efdaabef8e0b819d10dcbc
Jesse <jessebeke10@gmail.com>
2024-07-03 05:57:00 +0100

    feat: minor changes to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,11 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="flex justify-between items-center mb-6">
         <NavLink to="/">
-          <img
-            alt="SEES-Logo"
-            className="h-14 inline"
-            src="SEES.jpg"
-          ></img>
+          <img alt="SEES-Logo" className="h-14 inline" src="SEES.jpg"></img>
         </NavLink>
         <div className="flex flex-col text-left lg:w-100 md:w-11/12 w-[80%]">
           <h1 className="text-[18px] font-semibold">Tournament updates</h1>
@@ -17,6 +13,12 @@ export default function Navbar() {
             Get real-time updates on the scores
           </p>
         </div>
+        <NavLink
+          className="inline-flex bg-white text-white items-center mb-4 justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-white bg-background hover:bg-slate-100 h-9 rounded-md px-3"
+          to="/create"
+        >
+          Create Match
+        </NavLink>
       </nav>
     </div>
   );
