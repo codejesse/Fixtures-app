dbb01beb0728179ec0a32066e1eba866d833f359
Jesse <jessebeke10@gmail.com>
2024-07-03 06:06:19 +0100

    feat: navbar fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
           </p>
         </div>
         <NavLink
-          className="inline-flex bg-white text-white items-center mb-4 justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-white bg-background hover:bg-slate-100 h-9 rounded-md px-3"
+          className="hidden bg-white text-white items-center mb-4 justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-white bg-background hover:bg-slate-100 h-9 rounded-md px-3"
           to="/create"
         >
           Create Match
