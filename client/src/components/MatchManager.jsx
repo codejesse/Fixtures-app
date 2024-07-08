99ed1d53897f1a45c386d9c37ca764931564c51b
Jesse <jessebeke10@gmail.com>
2024-07-08 07:24:58 +0100

    feat: admin page ui modifications

--- client/src/components/MatchManager.jsx
+++ client/src/components/MatchManager.jsx
@@ -6,7 +6,7 @@ const socket = io('/');
 
 const Match = (props) => (
   <div className="border bg-white">
-    <p className="">{props.record.teamA}</p>
+    <p className="">{props.record.teamA} Vs {props.record.teamB}</p>
     <div className="flex gap-2">
       <Link
         className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3"
