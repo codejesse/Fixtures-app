0a3dfceb87efc8f1b262335a4c7cd58cdd38bb1c
Jesse <jessebeke10@gmail.com>
2024-07-08 07:22:07 +0100

    feat: server fix

--- server/server.js
+++ server/server.js
@@ -1,18 +1,9 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-const http = require("http");
-const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 5050;
 const app = express();
-const server = http.createServer(app);
-// export const io = new Server(server, {
-//   cors: {
-//     origin: "/record",
-//     methods: ["GET", "POST", "PATCH", "DELETE"],
-//   },
-// });
 
 app.use(cors());
 app.use(express.json());
